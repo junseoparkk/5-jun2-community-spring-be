755f7aa1803b40b7f4368f340eec10629da5270f
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-06-12 16:09:08 +0900

    test: 데이터베이스 연결 테스트 구현

--- src/test/java/com/kcs/community/database/DBConnectionTest.java
+++ src/test/java/com/kcs/community/database/DBConnectionTest.java
@@ -1,2 +1,21 @@
-package com.kcs.community.database;public class DBConnectionTest {
+package com.kcs.community.database;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootTest
+public class DBConnectionTest {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    void testConnection() {
+        String sql = "SELECT 1";
+        Integer result = jdbcTemplate.queryForObject(sql, Integer.class);
+        assertTrue(result != null && result == 1, "Successfully connected database : mysql");
+    }
 }
